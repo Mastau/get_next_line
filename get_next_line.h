669ad0e085b03940ca85ffbf3e62e3ed4a26d5c9
Thomas Arnaud <thomarna@student.42angouleme.fr>
2024-10-22 19:42:19 +0200

    Initial Commit (Created files & try to understand subject)

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thomarna <thomarna@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 17:59:15 by thomarna          #+#    #+#             */
+/*   Updated: 2024/10/22 18:01:32 by thomarna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char *get_next_line(int fd);
+
+#endif
+
