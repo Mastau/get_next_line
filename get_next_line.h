59bcbcec2971b797166ddf0edf4ff396f6eacf91
Thomas Arnaud <thomarna@student.42angouleme.fr>
2024-10-24 15:46:09 +0200

    J'ai joué a rocket league :/

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,17 @@
 /*   By: thomarna <thomarna@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 17:59:15 by thomarna          #+#    #+#             */
-/*   Updated: 2024/10/22 18:01:32 by thomarna         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:45:30 by thomarna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE = 42
+# endif
+
 
 #include <stdlib.h>
 #include <unistd.h>
